724dfefa159bca44bd943dd0fe3f1c8c5fd6cb33
Arkadiusz Nowakowski <arkadiusz.nowakowski@us.edu.pl>
2019-04-01 08:43:00 +0200

    Add project files.

---
+++ physx-boilerplate/laboratory-code.cpp
@@ -0,0 +1,99 @@
+#include <ctype.h>
+
+#pragma comment(lib, "PhysX3DEBUG_x64.lib")
+#pragma comment(lib, "PhysX3CommonDEBUG_x64.lib")
+#pragma comment(lib, "PxFoundationDEBUG_x64.lib")
+#pragma comment(lib, "PhysX3ExtensionsDEBUG.lib")
+#pragma comment(lib, "PxPvdSDKDEBUG_x64.lib")
+
+#include "PxPhysicsAPI.h"
+
+#include "SnippetPrint.h"
+#include "SnippetPVD.h"
+#include "SnippetUtils.h"
+
+
+using namespace physx;
+
+PxDefaultAllocator		gAllocator;
+PxDefaultErrorCallback	gErrorCallback;
+
+PxFoundation*			gFoundation = NULL;
+PxPhysics*				gPhysics	= NULL;
+
+PxDefaultCpuDispatcher*	gDispatcher = NULL;
+PxScene*				gScene		= NULL;
+
+PxMaterial*				gMaterial	= NULL;
+
+PxPvd*                  gPvd        = NULL;
+
+void initPhysics(bool interactive)
+{
+	gFoundation = PxCreateFoundation(PX_FOUNDATION_VERSION, gAllocator, gErrorCallback);
+
+	gPvd = PxCreatePvd(*gFoundation);
+	PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate(PVD_HOST, 5425, 10);
+	gPvd->connect(*transport,PxPvdInstrumentationFlag::eALL);
+
+	gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(),true,gPvd);
+
+	PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
+	sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
+	gDispatcher = PxDefaultCpuDispatcherCreate(2);
+	sceneDesc.cpuDispatcher	= gDispatcher;
+	sceneDesc.filterShader	= PxDefaultSimulationFilterShader;
+	gScene = gPhysics->createScene(sceneDesc);
+
+	PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
+	if(pvdClient)
+	{
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+	}
+	gMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f);
+	PxRigidStatic* groundPlane = PxCreatePlane(*gPhysics, PxPlane(0,1,0,0), *gMaterial);
+	gScene->addActor(*groundPlane);
+
+	PxShape* shape = gPhysics->createShape(PxBoxGeometry(10, 10, 10), *gMaterial);
+	PxRigidDynamic* body = gPhysics->createRigidDynamic(PxTransform(PxVec3(0,20,-20)));
+	body->attachShape(*shape);
+	PxRigidBodyExt::updateMassAndInertia(*body, 10.0f);
+	gScene->addActor(*body);
+}
+
+void stepPhysics(bool interactive)
+{
+	PX_UNUSED(interactive);
+	gScene->simulate(1.0f/60.0f);
+	gScene->fetchResults(true);
+}
+	
+void cleanupPhysics(bool interactive)
+{
+	PX_UNUSED(interactive);
+	gScene->release();
+	gDispatcher->release();
+	gPhysics->release();	
+	PxPvdTransport* transport = gPvd->getTransport();
+	gPvd->release();
+	transport->release();
+	gFoundation->release();
+}
+
+void keyPress(unsigned char key, const PxTransform& camera)
+{
+	switch(toupper(key))
+	{
+		// ob³uga klawiszy
+		// przyk³ad: case 'B': createStack(PxTransform(PxVec3(0,0,stackZ-=10.0f)), 10, 2.0f); break;
+	}
+}
+
+int snippetMain(int, const char*const*)
+{
+	extern void renderLoop();
+	renderLoop();
+	return 0;
+}
---
+++ physx-boilerplate/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+extern int snippetMain(int, const char*const*);
+
+int main(int argc, char** argv)
+{
+	return snippetMain(argc, argv);
+}
